9d7056a44fb5f947f02e4e3026134df69ff0061c
egumin <ethangumin@gmail.com>
2023-05-10 23:45:54 -0400

    Revert "comments"
    This reverts commit 4fb2b0e9a761d3cc159880be0aae82d70e72d4e9.

--- src/commands.rs
+++ src/commands.rs
@@ -1,25 +1,30 @@
 use std::{process::Command, collections::VecDeque};
 
-// takes a string consisting of 
-// "dir args..."
-// splits on whitespace and uses any extra args after the dir
+/*
+ * pub fn ls_arg(dir: &str, arg: &str) -> Vec<String> {
+ *     let mut res = vec![];
+ *     let mut cmd = Command::new("ls");
+ *     cmd.arg("-p").arg(arg).current_dir(dir);
+ *     let output = cmd.output().expect("failed to run");
+ *     let res_string: String = String::from_utf8(output.stdout).unwrap();
+ *     let lines = res_string.lines();
+ *     for l in lines {
+ *         res.push(l.to_string());
+ *     }
+ * 
+ *     return res;
+ * }
+ */
+
 pub fn ls(arg: &str) -> Vec<String> {
-    // use Deque to pop_front
     let mut args = arg.split(" ").collect::<VecDeque<&str>>();
-    // get dir to execute ls
     let dir = args.pop_front().unwrap();
     let mut res = vec![];
-
-    // start the process
     let mut cmd = Command::new("ls");
-    // add args
     cmd.arg("-p").args(args).current_dir(dir);
-
-    // prepare output
     let output = cmd.output().expect("failed to run");
     let res_string: String = String::from_utf8(output.stdout).unwrap();
     let lines = res_string.lines();
-    // build result vector
     for l in lines {
         res.push(l.to_string());
     }
